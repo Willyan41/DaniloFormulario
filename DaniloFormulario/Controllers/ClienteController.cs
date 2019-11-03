7bdcf6bed7ff77adc4531bbaedfb175bb42bb1aa
Willyan Santos <willyan41@hotmail.com>
2019-11-03 15:38:54 -0300

    Foi Implementado ClienteController

---
+++ DaniloFormulario/Controllers/ClienteController.cs
@@ -0,0 +1,142 @@
+﻿using DaniloFormulario.Models;
+using Domain.Entidade;
+using Domain.Gerenciador;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DaniloFormulario.Controllers
+{
+    public class ClienteController : BaseController
+    {
+        ClienteGerenciador clienteGerenciador;
+        private IHostingEnvironment _env;
+
+        public ClienteController(IHostingEnvironment env)
+        {
+            clienteGerenciador = new ClienteGerenciador();
+            _env = env;
+
+        }
+
+        public IActionResult Index()
+        {
+            var Clientes = clienteGerenciador.RecuperarCliente()
+                .Select(c => new ClienteViewModel()
+                {
+                    Id = c.Id,
+                    Nome = c.Nome,
+                    CPF = c.CPF,
+                    RG = c.RG,
+                    DataNascimento = c.DataNascimento,
+                    CNH = c.CNH,
+                    LocalNascimento = c.LocalNascimento,
+                    NomePai = c.NomePai,
+                    NomeMae = c.NomeMae,
+                    TempoResidencia = c.TempoResidencia,
+                });
+            var model = new ClienteViewModel { Clientes = Clientes };
+            return View(model);
+
+        }
+
+        [HttpGet]
+
+        public IActionResult Add(int Id)
+        {
+            var c = clienteGerenciador.RecuperarPorID(Id);
+            var model = new ClienteViewModel()
+            {
+                Id = c.Id,
+                Nome = c.Nome,
+                CPF = c.CPF,
+                RG = c.RG,
+                DataNascimento = c.DataNascimento,
+                CNH = c.CNH,
+                LocalNascimento = c.LocalNascimento,
+                NomePai = c.NomePai,
+                NomeMae = c.NomeMae,
+                TempoResidencia = c.TempoResidencia,
+
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult> Add(ClienteViewModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                Cliente c = null;
+
+                if (model.Id > 0)
+                    c = clienteGerenciador.RecuperarPorID(model.Id);
+                else
+                    c = new Cliente();
+
+                c.Id = model.Id;
+                c.Nome = model.Nome;
+                c.CPF = model.CPF;
+                c.RG = model.RG;
+                c.DataNascimento = model.DataNascimento;
+                c.CNH = model.CNH;
+                c.LocalNascimento = model.LocalNascimento;
+                c.NomePai = model.NomePai;
+                c.NomeMae = model.NomeMae;
+                c.TempoResidencia = model.TempoResidencia;
+
+                clienteGerenciador.Add(c);
+
+            }
+
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpGet]
+
+        public IActionResult Delete(int Id)
+        {
+            var cliente = clienteGerenciador.RecuperarPorID(Id);
+
+            if (cliente != null)
+            {
+                clienteGerenciador.Delete(cliente);
+
+                var clientes = clienteGerenciador.RecuperarCliente()
+                    .Select(c => new ClienteViewModel()
+                    {
+                        Id = c.Id,
+                        Nome = c.Nome,
+                        CPF = c.CPF,
+                        RG = c.RG,
+                        DataNascimento = c.DataNascimento,
+                        CNH = c.CNH,
+                        LocalNascimento = c.LocalNascimento,
+                        NomePai = c.NomePai,
+                        NomeMae = c.NomeMae,
+                        TempoResidencia = c.TempoResidencia,
+
+                    });
+                return PartialView("Cliente", clientes);
+
+            }
+
+            return BadRequest("Erro ao deletar Cliente.");
+
+
+
+
+        }
+
+
+
+
+
+    }
+}
---
+++ DaniloFormulario/Models/ClienteViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DaniloFormulario.Models
+{
+    public class ClienteViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int CPF { get; set; }
+        public int RG { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public Byte CNH { get; set; }
+        public string LocalNascimento { get; set; }
+        public string NomePai { get; set; }
+        public string NomeMae { get; set; }
+        public int TempoResidencia { get; set; }
+
+        public IQueryable<ClienteViewModel> Clientes { get; set; }
+    }
+}
